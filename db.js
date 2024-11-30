81b4eeb76eb126f666cdcd64a775f518c322c1b9
Danny Burrow <burrowingdan@gmail.com>
2024-11-29 22:34:21 -0800

    try extending timeout

--- db.js
+++ db.js
@@ -1,11 +1,12 @@
 import mongoose from "mongoose"
 
-const dbConnect = async () => {
+const dbConnect = async (req, res) => {
   try {
     await mongoose.connect(process.env.DB_URL)
     console.log(`[database]: connected to db`)
   } catch (err) {
     console.warn(`[database error]: ${err}`)
+    return res.status(500).json({ error: `[database error:] ${err}` })
   }
 }
 
